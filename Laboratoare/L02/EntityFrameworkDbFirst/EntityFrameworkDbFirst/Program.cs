3ba8037d70eaee190e733979811034dcdda18c2e
Ovidiu Rudi <djvidov@yahoo.com>
2020-07-16 21:08:51 +0300

    am lucrat la laboratorul 2

---
+++ Laboratoare/L02/EntityFrameworkDbFirst/EntityFrameworkDbFirst/Program.cs
@@ -0,0 +1,34 @@
+﻿using EntityFrameworkDbFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDbFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Supplier s = new Supplier();
+            CRMEntities db = new CRMEntities();
+            db.Database.Log += logQuery;
+
+            List<Supplier> suppliers = db.Suppliers.ToList();
+
+            List<Customer> customers = db.Customers.ToList();
+
+            List<Order> ordersOver1000 = db.Orders.Where(o => o.TotalAmount > 1000).ToList();
+
+            Supplier s1 = db.Suppliers.FirstOrDefault();
+
+            Console.ReadKey();
+        }
+
+        private static void logQuery(string log)
+        {
+            Console.WriteLine(log);
+        }
+    }
+}
