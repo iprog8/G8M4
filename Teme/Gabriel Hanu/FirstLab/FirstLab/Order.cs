f206a89ab2e0ba19d7bb9d0e3d9f8328e0bbf17a
HiMyNameIsGarch <gabrielhnau700@yahoo.com>
2020-07-16 12:33:39 +0300

    extragere date din baza de date

---
+++ Teme/Gabriel Hanu/FirstLab/FirstLab/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLab
+{
+    public class Order
+    {
+        public Order(int id, DateTime orderDate, string orderNumber, int customerId, decimal totalAmount)
+        {
+            Id = id;
+            OrderDate = orderDate;
+            OrderNumber = orderNumber;
+            CustomerId = customerId;
+            TotalAmount = totalAmount;
+        }
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string OrderNumber { get; set; }
+        public int CustomerId { get; set; }
+        public decimal TotalAmount { get; set; }
+
+    }
+}
