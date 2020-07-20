c307cca5d1074e2f4df18d4db5461dd87488b8f3
avramcristian <whiskyman_cv@yahoo.com>
2020-07-20 20:49:53 +0300

    SocialMedia

---
+++ Teme/Avram Cristian/SocialMedia/SocialMedia/Models/UtilizatorEXT.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+    public partial class Utilizator
+    {
+        public string Address { get; set; }
+    }
+}
