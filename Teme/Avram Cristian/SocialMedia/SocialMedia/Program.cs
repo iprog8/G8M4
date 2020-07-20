e1e8a5fce5bfd9d178dfacd0346676e2f073a231
avramcristian <whiskyman_cv@yahoo.com>
2020-07-20 20:49:53 +0300

    SocialMedia

---
+++ Teme/Avram Cristian/SocialMedia/SocialMedia/Program.cs
@@ -0,0 +1,23 @@
+﻿using SocialMedia.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia
+{
+    class Program
+    {
+         
+        static void Main(string[] args)
+        {
+            Social_MediaEntities db = new Social_MediaEntities();
+            ICollection<Utilizator> utilizatori = db.Utilizators.ToList();
+            ICollection<Postare> postari = db.Postares.ToList();
+            Console.ReadKey();
+            Utilizator u = new Utilizator();
+            u.Address = "Craiova";
+        }
+    }
+}
