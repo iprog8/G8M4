834baf9495f249d31368c702b75fd79a70d65e1b
BogdanG93 <bogdan_geagu93@yahoo.com>
2020-07-22 22:13:41 +0300

    tema 03

---
+++ Teme/Bogdan G/L03/EFandLambdaExpressions/EFandLambdaExpressions/Comenzi.cs
@@ -0,0 +1,35 @@
+﻿using EFandLambdaExpressions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFandLambdaExpressions
+{
+    public class Comenzi
+    {
+        public ICollection<Order> GetAll()
+        {
+            CRMEntities db = new CRMEntities();
+            ICollection<Order> comenzi = db.Order.ToList();
+            Console.WriteLine("Comenzile mai mari de 5000 au ClientID si suma:");
+            foreach ( var comanda in comenzi.Where(o => o.TotalAmount >= 5000))
+            {
+                if (comanda != null)
+                {
+                    Console.WriteLine($" {comanda.CustomerId} - {comanda.TotalAmount}");
+                }
+            }
+            return comenzi;
+        }
+
+        public ICollection<Order> GetById(int customerId)
+        {
+            CRMEntities db = new CRMEntities();
+            ICollection<Order> comenziClient = db.Order.Where(o => o.CustomerId == customerId).ToList();
+            Console.WriteLine($"Numarul de comenzi ale clientului cu id {customerId} este : {comenziClient.Count}");
+            return comenziClient;
+        }
+    }
+}
---
+++ Teme/Bogdan G/L03/EFandLambdaExpressions/EFandLambdaExpressions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFandLambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            /*client.Insert("Mioveni", "Romania", "Geagu", "Bogdan", "0770648465");
+            client.Insert("RosioriDeVede", "Romania", "Pirvan", "Cristiana", "0720444584");*/
+            client.Update(5, "Ion", "Ion");
+            Comenzi comenzi = new Comenzi();
+            comenzi.GetAll();
+            comenzi.GetById(14);
+            client.SelectByCity("Paris");
+            Console.ReadKey();
+        }
+    }
+}
