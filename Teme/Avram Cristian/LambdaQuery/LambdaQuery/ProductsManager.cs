f45bd8dae69d3f5f404823ef8ea67605a2f10bc1
avramcristian <whiskyman_cv@yahoo.com>
2020-07-22 15:27:56 +0300

    FewQueries

---
+++ Teme/Avram Cristian/LambdaQuery/LambdaQuery/ProductsManager.cs
@@ -0,0 +1,21 @@
+﻿using LambdaQuery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaQuery
+{
+    class ProductsManager
+    {
+        /*public ICollection<Product> GetTop(int top)
+        {
+            CRMEntities db = new CRMEntities();
+            ICollection<Product> productsTop = db.Products.Where(p => p.UnitPrice Nu gasesc sintaxa pentru SELECT TOP corespunzatoare C# iar acum ma duc la un gratar! Iar de maine...lucrez :().ToList();
+            return productsTop;
+
+
+        }*/
+    }
+}
---
+++ Teme/Avram Cristian/LambdaQuery/LambdaQuery/SupplierManager.cs
@@ -0,0 +1,37 @@
+﻿using LambdaQuery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaQuery
+{
+    public class SupplierManager
+    {
+        public void Insert(string company, string contact, string city, string country)
+        {
+            CRMEntities db = new CRMEntities();
+            Supplier supplier = new Supplier();
+            supplier.City = city;
+            supplier.CompanyName = company;
+            supplier.ContactName = contact;
+            supplier.Country = country;
+            db.Suppliers.Add(supplier);
+            db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            CRMEntities db = new CRMEntities();
+            Supplier toBeDeleted = db.Suppliers.FirstOrDefault(s => s.Id == id);
+            if (toBeDeleted != null)
+            {
+                db.Suppliers.Remove(toBeDeleted);
+                db.SaveChanges();
+            }
+        }
+
+        
+    }
+}
