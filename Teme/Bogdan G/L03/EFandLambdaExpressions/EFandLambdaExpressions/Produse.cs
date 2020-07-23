9c32282b6e03cfc0a3cd38ee19d92df437a44539
BogdanG93 <bogdan_geagu93@yahoo.com>
2020-07-23 18:00:57 +0300

    update L03

---
+++ Teme/Bogdan G/L03/EFandLambdaExpressions/EFandLambdaExpressions/Produse.cs
@@ -0,0 +1,32 @@
+﻿using EFandLambdaExpressions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFandLambdaExpressions
+{
+    public class Produse
+    {
+        public ICollection<Product> CeleMaiScumpe()
+        {
+            CRMEntities db = new CRMEntities();
+            ICollection<Product> produse = db.Product.ToList();
+            ushort maxProduse = 4;
+            foreach (var costProdus in produse.Where(p => p.UnitPrice > p.UnitPrice))
+            {
+                if(db.Product.ToList().Count <= maxProduse)
+                {
+                    db.Product.ToList().Add(costProdus);
+                }
+                else
+                {
+                    db.Product.ToList().RemoveAt(0);
+                    db.Product.ToList().Add(costProdus);
+                }
+            }
+            return produse;
+        }
+    }
+}
