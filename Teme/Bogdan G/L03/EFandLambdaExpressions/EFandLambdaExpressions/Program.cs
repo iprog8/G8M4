d6bd9745660b13f94356bbf7ec886b008a89ecab
BogdanG93 <bogdan_geagu93@yahoo.com>
2020-07-23 18:00:57 +0300

    update L03

--- Teme/Bogdan G/L03/EFandLambdaExpressions/EFandLambdaExpressions/Program.cs
+++ Teme/Bogdan G/L03/EFandLambdaExpressions/EFandLambdaExpressions/Program.cs
@@ -10,14 +10,16 @@ namespace EFandLambdaExpressions
     {
         static void Main(string[] args)
         {
-            Client client = new Client();
-            /*client.Insert("Mioveni", "Romania", "Geagu", "Bogdan", "0770648465");
-            client.Insert("RosioriDeVede", "Romania", "Pirvan", "Cristiana", "0720444584");*/
+            /*Client client = new Client();
+            client.Insert("Mioveni", "Romania", "Geagu", "Bogdan", "0770648465");
+            client.Insert("RosioriDeVede", "Romania", "Pirvan", "Cristiana", "0720444584");
             client.Update(5, "Ion", "Ion");
             Comenzi comenzi = new Comenzi();
             comenzi.GetAll();
             comenzi.GetById(14);
-            client.SelectByCity("Paris");
+            client.SelectByCity("Paris");*/
+            Produse produs = new Produse();
+            produs.CeleMaiScumpe();
             Console.ReadKey();
         }
     }
