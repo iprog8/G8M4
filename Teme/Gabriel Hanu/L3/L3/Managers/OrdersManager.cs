ca0da9deec40982f5fa1f32fde4424c4684bff8b
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2020-07-25 21:03:32 +0300

    tema restanta

---
+++ Teme/Gabriel Hanu/L3/L3/Managers/OrdersManager.cs
@@ -0,0 +1,20 @@
+﻿using L3.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L3.Managers
+{
+    public class OrdersManager
+    {
+        public ICollection<Order> GetAllByTAOver(int totalAmount)
+        {
+            CRMEntities db = new CRMEntities();
+            ICollection<Order> toBeSelected = db.Orders.Where(o => o.TotalAmount > totalAmount).ToList();
+            return toBeSelected;
+        }
+    }
+}
