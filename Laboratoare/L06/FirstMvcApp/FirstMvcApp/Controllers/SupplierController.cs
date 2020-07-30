4927876edc8d4f2b58f933b38fbdd134880c08b8
Ovidiu Rudi <djvidov@yahoo.com>
2020-07-30 22:18:13 +0300

    am lucrat in laboratorul 06

---
+++ Laboratoare/L06/FirstMvcApp/FirstMvcApp/Controllers/SupplierController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstMvcApp.Models;
+
+namespace FirstMvcApp.Controllers
+{
+    public class SupplierController : Controller
+    {
+        // GET: Supplier
+        public ActionResult Index()
+        {
+            CRMEntities db = new CRMEntities();
+            ICollection<Supplier> suppliers = db.Suppliers.ToList();
+
+            return View(suppliers);
+        }
+    }
+}
