6beadf604f07315332259630ceea24a1949aeaf1
HiMyNameIsGarch <gabrielhnau700@yahoo.com>
2020-08-06 20:47:27 +0300

    Partea a doua a temei

---
+++ Teme/Gabriel Hanu/CrmManager/CrmManager/Managers/SupplierManager.cs
@@ -0,0 +1,60 @@
+﻿using CrmManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmManager.Managers
+{
+    public class SupplierManager
+    {
+        public void Display()
+        {
+            CRMEntities db = new CRMEntities();
+            ICollection<Supplier> suppliers = db.Suppliers.ToList();
+            foreach (var supplier in suppliers)
+            {
+                Console.WriteLine($"Id-ul: {supplier.Id} \nNumele companiei: {supplier.CompanyName} \nNumele de contact: {supplier.ContactName} \nTara: {supplier.Country} \nOrasul: {supplier.City}");
+            }
+        }
+        public void Add(Supplier supplier)
+        {
+            CRMEntities db = new CRMEntities();
+            db.Suppliers.Add(supplier);
+            db.SaveChanges();
+        }
+        public bool Delete(int id)
+        {
+            CRMEntities db = new CRMEntities();
+            Supplier toBeDeleted = db.Suppliers.Find(id);
+            if (toBeDeleted == null) return false;
+            db.Suppliers.Remove(toBeDeleted);
+            db.SaveChanges();
+            return true;
+        }
+        public bool VerifyId(string id)
+        {
+            if(int.TryParse(id, out int newId))
+            {
+                if (newId < 1) return false;
+                else if(new CRMEntities().Suppliers.Find(newId) != null)
+                return true;
+            }
+            return false;
+        }
+        public void Modify(int id, string companyName, string contactName, string contactTitle, string city, string country, string phone)
+        {
+            CRMEntities db = new CRMEntities();
+            Supplier toBeUpdated = db.Suppliers.Find(id);
+            if (toBeUpdated == null) return;
+            toBeUpdated.CompanyName = companyName;
+            toBeUpdated.ContactName = contactName;
+            toBeUpdated.ContactTitle = contactTitle;
+            toBeUpdated.City = city;
+            toBeUpdated.Country = country;
+            toBeUpdated.Phone = phone;
+            db.SaveChanges();
+        }
+    }
+}
--- Teme/Gabriel Hanu/CrmManager/CrmManager/Program.cs
+++ Teme/Gabriel Hanu/CrmManager/CrmManager/Program.cs
@@ -22,8 +22,9 @@ namespace CrmManager
             Implementati fieccare functionalitate.*/
         static void Main(string[] args)
         {
-            //Create the managers for the customers and suppliers
             CustomersManager customersManager = new CustomersManager();
+            SupplierManager supplierManager = new SupplierManager();
+            //Create the managers for the customers and suppliers
 
             //Display the options for the user to choose
             Console.WriteLine("Bun venit la aplicatia noastra CRM, \nCe actiune vreti sa faceti?\n");
@@ -82,17 +83,97 @@ namespace CrmManager
                     }
                     break;
                 case ConsoleKey.D5:
+                    supplierManager.Display();
                     break;
                 case ConsoleKey.D6:
+                    Supplier tempSupplier = new Supplier();
+                    Console.WriteLine($"Introduceti numele companiei!");
+                    tempSupplier.CompanyName = ProcessText("Numele companiei");
+                    Console.WriteLine("Introduceti numele de contact");
+                    tempSupplier.ContactName = ProcessText("Numele de contact");
+                    Console.WriteLine("Introduceti titlul de contact");
+                    tempSupplier.ContactTitle = ProcessText("Titlul de contact");
+                    Console.WriteLine("Introduceti orasul dvs");
+                    tempSupplier.City = ProcessText("Introduceti orasul");
+                    Console.WriteLine("Introduceti tara dvs");
+                    tempSupplier.Country = ProcessText("Introduceti tara");
+                    Console.WriteLine("Introduceti numarul de telefon!");
+                    tempSupplier.Phone = ProcessNumber("Numar de telefon");
+                    supplierManager.Add(tempSupplier);
+                    Console.WriteLine("Furnizorul a fost adaugat!");
                     break;
                 case ConsoleKey.D7:
+                    Console.WriteLine("Introduceti id-ul pe care vreti sa il modificati");
+                    int suppId = Geopt(supplierManager);
+                    Supplier tempSupp = new CRMEntities().Suppliers.Find(suppId);
+                    Console.WriteLine($"Numele companiei: {tempSupp.CompanyName} \nNumele de contact: {tempSupp.ContactName} \nTitlul de contact: {tempSupp.ContactTitle} \nOrasul: {tempSupp.City} \nTara: {tempSupp.Country} \nTelefon: {tempSupp.Phone}");
+                    Console.WriteLine("Introduceti noul nume al companiei:");
+                    string CompanyName = ProcessText("Numele companiei");
+                    Console.WriteLine("Introduceti noul nume de contact:");
+                    string ContactName = ProcessText("Numele de contact");
+                    Console.WriteLine("Introduceti noul nume noul titlu de contact:");
+                    string ContactTitle = ProcessText("Titlul de contact");
+                    Console.WriteLine("Introduceti noul oras:");
+                    string City = ProcessText("Oras");
+                    Console.WriteLine("Introduceti noua tara:");
+                    string Country = ProcessText("Tara");
+                    Console.WriteLine("Introduceti noul telefon:");
+                    string Phone = ProcessNumber("Telefon");
+                    supplierManager.Modify(suppId, CompanyName, ContactName, ContactTitle, City, Country, Phone);
+                    Console.WriteLine("Furnizorul a fost modificat cu succes!");
                     break;
                 case ConsoleKey.D8:
+                    Console.WriteLine("Introduceti id-ul pe care vreti sa il stergeti!");
+                    supplierManager.Delete(Geopt(supplierManager));
+                    Console.WriteLine("Furnizorul a fost sters!");
                     break;
                 default:
                     break;
             }
             Console.ReadKey();
+        }
+        private static string ProcessText(string nameOfValue)
+        {
+            string value = Console.ReadLine();
+            foreach (char c in value)
+            {
+                if (char.IsWhiteSpace(c)) continue;
+                else if (!char.IsLetter(c))
+                {
+                    Console.WriteLine($"{nameOfValue} nu contine doar litere, mai incearca...");
+                    return ProcessText(nameOfValue);
+                }
+            }
+            if (!char.IsUpper(value.FirstOrDefault()))
+            {
+                Console.WriteLine($"{nameOfValue} nu incepe cu o litera mare, mai incearca...");
+                return ProcessText(nameOfValue);
+            }
+            return value;
+        }
+        private static string ProcessNumber(string nameOfValue)
+        {
+            string value = Console.ReadLine();
+            foreach (char c in value)
+            {
+                if (char.IsWhiteSpace(c)) continue;
+                else if (!char.IsDigit(c))
+                {
+                    Console.WriteLine($"{nameOfValue} nu contine doar numere, mai incearca...");
+                    return ProcessNumber(nameOfValue);
+                }
+            }
+           return value;
+        }
+        public static int Geopt(SupplierManager supplierManager)
+        {
+            string id = Console.ReadLine();
+            if (!supplierManager.VerifyId(id))
+            {
+                Console.WriteLine("Id-ul pe care l-ati introdus este inval id, mai incercati...");
+                return Geopt(supplierManager);
+            }
+            return int.Parse(id);
         }
     }
 }
