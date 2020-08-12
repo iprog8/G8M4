5260edad95997e2131b77053c962b98ff04b17df
HiMyNameIsGarch <gabrielhnau700@yahoo.com>
2020-08-12 20:01:33 +0300

    crm manager with mvc

---
+++ Teme/Gabriel Hanu/CrmManagerMvc/CrmManagerMvc/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using CrmManagerMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrmManagerMvc.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            CRMEntities db = new CRMEntities();
+            ICollection<Customer> customers = db.Customers.ToList();
+            return View(customers);
+        }
+        // GET: Details
+        public ActionResult Details(int id)
+        {
+            CRMEntities db = new CRMEntities();
+            Customer model = db.Customers.Find(id);
+            if (model == null)
+                return HttpNotFound();
+            return View(model);
+        }
+    }
+}
---
+++ Teme/Gabriel Hanu/CrmManagerMvc/CrmManagerMvc/Controllers/SupplierController.cs
@@ -0,0 +1,27 @@
+﻿using CrmManagerMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrmManagerMvc.Controllers
+{
+    public class SupplierController : Controller
+    {
+        // GET: Supplier
+        [HttpGet]
+        public ActionResult Index(int nrPagina)
+        {
+            int elemsToTake = 10;
+            CRMEntities db = new CRMEntities();
+            if(db.Suppliers.Count() / 10 < nrPagina || nrPagina < 0) return HttpNotFound();
+            ICollection<Supplier> model = db.Suppliers
+                .OrderBy(s => s.CompanyName)
+                .Skip(nrPagina - 1)
+                .Take(elemsToTake)
+                .ToList();
+            return View(model);
+        }
+    }
+}
