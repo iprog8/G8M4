a2ebff789dd05e544651267a8c4a6b44925d91a7
avramcristian <whiskyman_cv@yahoo.com>
2020-08-13 00:27:28 +0300

    CustomersMVC

---
+++ Teme/Avram Cristian/CustomerMVC/CustomerMVC/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CustomerMVC;
+using CustomerMVC.Models;
+
+namespace CustomerMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            CRMEntities db = new CRMEntities();
+            ICollection<Customer> customers = db.Customers.ToList();
+
+            return View(customers);
+        }
+    }
+}
