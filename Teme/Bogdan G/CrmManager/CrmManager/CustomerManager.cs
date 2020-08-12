24941b05f66ddcec3622284f28c94b0825718ddf
BogdanG93 <bogdan_geagu93@yahoo.com>
2020-08-12 19:04:21 +0300

    CrmManager

---
+++ Teme/Bogdan G/CrmManager/CrmManager/CustomerManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmManager.Managers
+{
+    public class CustomerManager
+    {
+        public void Display()
+        {
+            CRMEntities db = new CRMEntities();
+            ICollection<Customer> customers = db.Customer.ToList();
+            foreach (var customer in customers)
+            {
+                Console.WriteLine($"Id-ul: {customer.Id} \nNumele clientului: {customer.FirstName} {customer.LastName} \nOrasul: {customer.City} \nTara: {customer.Country}");
+            }
+        }
+        public void Add(Customer customer)
+        {
+            CRMEntities db = new CRMEntities();
+            db.Customer.Add(customer);
+            db.SaveChanges();
+        }
+        public void Modify(int id, string firstName, string lastName, string city, string country)
+        {
+            CRMEntities db = new CRMEntities();
+            Customer toBeUpdated = db.Customer.Find(id);
+            if (toBeUpdated == null) return;
+            toBeUpdated.FirstName = firstName;
+            toBeUpdated.LastName = lastName;
+            toBeUpdated.City = city;
+            toBeUpdated.Country = country;
+            db.SaveChanges();
+        }
+        public bool VerifyId(string id)
+        {
+            if (int.TryParse(id, out int newId))
+            {
+                if (newId < 1) return false;
+                else if (new CRMEntities().Customer.Find(newId) != null)
+                    return true;
+            }
+            return false;
+        }
+        public Customer GetCustomerById(int id)
+        {
+            return new CRMEntities().Customer.Find(id);
+        }
+        public void Delete(int id)
+        {
+            CRMEntities db = new CRMEntities();
+            Customer toBeDeleted = db.Customer.Find(id);
+            if (toBeDeleted == null) return;
+            db.Customer.Remove(toBeDeleted);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ Teme/Bogdan G/CrmManager/CrmManager/SupplierManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmManager.Managers
+{
+    public class SupplierManager
+    {
+        public void Display()
+        {
+            CRMEntities db = new CRMEntities();
+            ICollection<Supplier> suppliers = db.Supplier.ToList();
+            foreach (var supplier in suppliers)
+            {
+                Console.WriteLine($"Id-ul: {supplier.Id} \nNumele companiei: {supplier.CompanyName} \nNumele de contact: {supplier.ContactName} \nTara: {supplier.Country} \nOrasul: {supplier.City}");
+            }
+        }
+        public void Add(Supplier supplier)
+        {
+            CRMEntities db = new CRMEntities();
+            db.Supplier.Add(supplier);
+            db.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            CRMEntities db = new CRMEntities();
+            Supplier toBeDeleted = db.Supplier.Find(id);
+            if (toBeDeleted == null) return;
+            db.Supplier.Remove(toBeDeleted);
+            db.SaveChanges();
+        }
+        public bool VerifyId(string id)
+        {
+            if (int.TryParse(id, out int newId))
+            {
+                if (newId < 1) return false;
+                else if (new CRMEntities().Supplier.Find(newId) != null)
+                    return true;
+            }
+            return false;
+        }
+        public void Modify(int id, string companyName, string contactName, string contactTitle, string city, string country, string phone)
+        {
+            CRMEntities db = new CRMEntities();
+            Supplier toBeUpdated = db.Supplier.Find(id);
+            if (toBeUpdated == null) return;
+            toBeUpdated.CompanyName = companyName;
+            toBeUpdated.ContactName = contactName;
+            toBeUpdated.ContactTitle = contactTitle;
+            toBeUpdated.City = city;
+            toBeUpdated.Country = country;
+            toBeUpdated.Phone = phone;
+            db.SaveChanges();
+        }
+    }
+}
