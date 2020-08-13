b589303acf6a11d07dac90151b9d8f839181d952
HiMyNameIsGarch <gabrielhnau700@yahoo.com>
2020-08-13 10:59:46 +0300

    bonus: paging

--- Teme/Gabriel Hanu/CrmManagerMvc/CrmManagerMvc/Controllers/SupplierController.cs
+++ Teme/Gabriel Hanu/CrmManagerMvc/CrmManagerMvc/Controllers/SupplierController.cs
@@ -1,4 +1,5 @@
 ﻿using CrmManagerMvc.Models;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,13 +16,22 @@ namespace CrmManagerMvc.Controllers
         {
             int elemsToTake = 10;
             CRMEntities db = new CRMEntities();
-            if(db.Suppliers.Count() / 10 < nrPagina || nrPagina < 0) return HttpNotFound();
+            if (db.Suppliers.Count() / 10 < nrPagina || nrPagina < 0) return HttpNotFound();
             ICollection<Supplier> model = db.Suppliers
                 .OrderBy(s => s.CompanyName)
                 .Skip(nrPagina - 1)
                 .Take(elemsToTake)
                 .ToList();
             return View(model);
+        }
+        [HttpGet]
+        public ActionResult Paging(int pageNumber)
+        {
+            int pageSize = 10;
+            CRMEntities db = new CRMEntities();
+            if (db.Suppliers.Count() / 10 < pageNumber || pageNumber < 0) return HttpNotFound();
+            ICollection<Supplier> model = db.Suppliers.ToList();
+            return View(model.ToPagedList(pageNumber, pageSize));
         }
     }
 }
