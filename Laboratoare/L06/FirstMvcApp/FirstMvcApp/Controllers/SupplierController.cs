b32d69451553c415cff560f94980b8773b60d3cc
Ovidiu Rudi <djvidov@yahoo.com>
2020-08-13 18:09:30 +0300

    am lucrat la laboratorul 07

--- Laboratoare/L06/FirstMvcApp/FirstMvcApp/Controllers/SupplierController.cs
+++ Laboratoare/L06/FirstMvcApp/FirstMvcApp/Controllers/SupplierController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,9 +14,37 @@ namespace FirstMvcApp.Controllers
         public ActionResult Index()
         {
             CRMEntities db = new CRMEntities();
-            ICollection<Supplier> suppliers = db.Suppliers.ToList();
+            ICollection<Supplier> model = db.Suppliers.Where(s => s.CompanyName.Contains("a")).ToList();
 
-            return View(suppliers);
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            CRMEntities db = new CRMEntities();
+            Supplier model = db.Suppliers.Find(id);
+            if (model == null)
+                return HttpNotFound();
+            return View(model);
+        }
+
+        [HttpPost]
+        public void Edit(Supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                CRMEntities db = new CRMEntities();
+                if(db.Suppliers.Any(s => s.Id == supplier.Id)) {
+                    db.Entry<Supplier>(supplier).State = EntityState.Modified;
+                    db.SaveChanges();
+                    RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                //return ModelState.
+            }
         }
     }
 }
