80fd289386a02b3b0fdef7ac4b68e92f4eb0783e
Ovidiu Rudi <djvidov@yahoo.com>
2020-08-13 21:58:44 +0300

    am lucrat la laboratorul 08

--- Laboratoare/L06/FirstMvcApp/FirstMvcApp/App_Start/RouteConfig.cs
+++ Laboratoare/L06/FirstMvcApp/FirstMvcApp/App_Start/RouteConfig.cs
@@ -14,10 +14,16 @@ namespace FirstMvcApp
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Multilanguage",
+                url: "{lang}/{controller}/{action}/{id}",
+                defaults: new { lang = "ro", controller = "Home", action = "Index", id = UrlParameter.Optional }
+                );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Supplier", action = "Index", id = UrlParameter.Optional }
             );
+
         }
     }
 }
--- Laboratoare/L06/FirstMvcApp/FirstMvcApp/Controllers/HomeController.cs
+++ Laboratoare/L06/FirstMvcApp/FirstMvcApp/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace FirstMvcApp.Controllers
             return View();
         }
 
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,9 +25,36 @@ namespace FirstMvcApp.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Contact2()
         {
-            ViewBag.Message = "Your contact page.";
+            int numar = new Random().Next(1, 100);
+            if (numar % 2 == 0)
+            {
+                return View("afiseazaLacuste");
+            }
+            else
+            {
+                return View("afiseazaBroaste");
+            }
+        }
+            public ActionResult Contact()
+        {
+            var language = this.RouteData.Values.FirstOrDefault(s => s.Key == "lang");
+            if (language.Value.ToString() == "ro")
+            {
+                ViewBag.Title = "Contact Neata!";
+                ViewBag.Message = "Pagina ta de contact";
+            }
+            else if(language.Value.ToString() == "en")
+            {
+                ViewBag.Title = "Good morning!";
+                ViewBag.Message = "Your contact page.";
+            }
+            else if (language.Value.ToString() == "fr")
+            {
+                ViewBag.Title = "Bon matine!";
+                ViewBag.Message = "NU stiu franceza";
+            }
 
             return View();
         }
