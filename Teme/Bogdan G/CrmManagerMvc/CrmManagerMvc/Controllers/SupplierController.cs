74262853ced0875580e734000ec328a1d1d44962
BogdanG93 <bogdan_geagu93@yahoo.com>
2020-08-16 18:58:08 +0300

    tema 7 incompleta

---
+++ Teme/Bogdan G/CrmManagerMvc/CrmManagerMvc/Controllers/SupplierController.cs
@@ -0,0 +1,37 @@
+﻿using CrmManagerMvc.Models;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrmManagerMvc.Controllers
+{
+    public class SupplierController : Controller
+    {
+        // GET: Supplier
+        [HttpGet]
+        public ActionResult Index(int nrPagina)
+        {
+            int elemsToTake = 10;
+            CRMEntities db = new CRMEntities();
+            if (db.Supplier.Count() / 10 < nrPagina || nrPagina < 0) return HttpNotFound();
+            ICollection<Supplier> model = db.Supplier
+                .OrderBy(s => s.CompanyName)
+                .Skip(nrPagina*elemsToTake - 1)
+                .Take(elemsToTake)
+                .ToList();
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult Paging(int pageNumber)
+        {
+            int pageSize = 10;
+            CRMEntities db = new CRMEntities();
+            if (db.Supplier.Count() / 10 < pageNumber || pageNumber < 0) return HttpNotFound();
+            ICollection<Supplier> model = db.Supplier.ToList();
+            return View(model.ToPagedList(pageNumber, pageSize));
+        }
+    }
+}
