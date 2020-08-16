21244973c220c60dcf87f12979d564641615a1f2
avramcristian <whiskyman_cv@yahoo.com>
2020-08-16 22:05:10 +0300

    New Routes

--- Teme/Avram Cristian/CustomerMVC/CustomerMVC/App_Start/RouteConfig.cs
+++ Teme/Avram Cristian/CustomerMVC/CustomerMVC/App_Start/RouteConfig.cs
@@ -14,9 +14,17 @@ namespace CustomerMVC
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                name: "Multilanguage",
+                url: "{lang}/{controller}/{action}/{id}",
+                defaults: new { lang = "ro", controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+          
+
+            routes.MapRoute(
+                name: "Multilanguage2",
+                url: "{lang}/{controller}/{action}/{id}",
+                defaults: new { lang = "en", controller = "Customers", action = "Index2", id = UrlParameter.Optional }
             );
         }
     }
--- Teme/Avram Cristian/CustomerMVC/CustomerMVC/Controllers/HomeController.cs
+++ Teme/Avram Cristian/CustomerMVC/CustomerMVC/Controllers/HomeController.cs
@@ -8,7 +8,14 @@ namespace CustomerMVC.Controllers
 {
     public class HomeController : Controller
     {
+       
         public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult Index2()
         {
             return View();
         }
@@ -22,7 +29,26 @@ namespace CustomerMVC.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            var language = this.RouteData.Values.FirstOrDefault(s => s.Key == "lang");
+
+            if (language.Value.ToString() == "ro")
+            {
+                ViewBag.Title = "Contact Neata";
+                ViewBag.Message = "romaneste";
+            } 
+            else if (language.Value.ToString() == "en") 
+            {
+                ViewBag.Title = "Godd morning!!!";
+                ViewBag.Message = "engleza";
+            }
+            else if (language.Value.ToString() == "fr")
+            {
+                ViewBag.Title = "Tres bon";
+                ViewBag.Message = "franceza";
+            }
+
+
+               
 
             return View();
         }
