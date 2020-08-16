84997a41482e97a55977ca1ae8d870125e76aab6
Vlad Dolinschi <vlad.dolinschi@gmail.com>
2020-08-16 20:18:52 +0300

    tema CRM pagination

---
+++ Teme/Vlad/L5 CRM Site/SuppliersManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L5_CRM_Site
+{
+    public class SuppliersManager
+    {
+        public string companyName { get; set; }
+        public string contactName { get; set; }
+        public Nullable<int> products { get; set; }
+    }
+}
