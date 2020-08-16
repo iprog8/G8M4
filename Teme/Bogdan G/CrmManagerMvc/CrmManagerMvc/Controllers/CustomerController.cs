4aaf0995436f5e64b1479830ebf46a14f6e20d9f
BogdanG93 <bogdan_geagu93@yahoo.com>
2020-08-16 18:58:08 +0300

    tema 7 incompleta

---
+++ Teme/Bogdan G/CrmManagerMvc/CrmManagerMvc/Controllers/CustomerController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CrmManagerMvc.Models;
+
+namespace CrmManagerMvc.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            CRMEntities db = new CRMEntities();
+            ICollection<Customer> customer = db.Customer.ToList();
+            return View(customer);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            CRMEntities db = new CRMEntities();
+            Customer model = db.Customer.Find(id);
+            if(model == null)
+                return HttpNotFound();
+            return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            CRMEntities db = new CRMEntities();
+            Customer model = db.Customer.Find(id);
+            if (model == null)
+                return HttpNotFound();
+            return View(model);
+        }
+
+        [HttpPost]
+        public void Edit(Customer customer)
+        {
+            if(ModelState.IsValid)
+            {
+                CRMEntities db = new CRMEntities();
+                if(db.Customer.Any(c => c.Id == customer.Id))
+                {
+                    db.Entry<Customer>(customer).State = EntityState.Modified;
+                    db.SaveChanges();
+                    RedirectToAction("Index");
+                }
+                else
+                {
+                    //return ModelState.
+                }
+            }
+        }
+    }
+}
---
+++ Teme/Bogdan G/CrmManagerMvc/CrmManagerMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CrmManagerMvc.MvcApplication" Language="C#" %>
