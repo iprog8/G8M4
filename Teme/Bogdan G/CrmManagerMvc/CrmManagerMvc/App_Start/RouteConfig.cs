e8a0967784b3a366f782247792a43eb44a601108
BogdanG93 <bogdan_geagu93@yahoo.com>
2020-08-17 20:31:11 +0300

    modificare ruta

--- Teme/Bogdan G/CrmManagerMvc/CrmManagerMvc/App_Start/RouteConfig.cs
+++ Teme/Bogdan G/CrmManagerMvc/CrmManagerMvc/App_Start/RouteConfig.cs
@@ -14,10 +14,17 @@ namespace CrmManagerMvc
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Multilanguage",
+                url: "{lang}/{controller}/{action}/{id}",
+                defaults: new { lang = "ro", controller = "Home", action = "Index", id = UrlParameter.Optional }
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
         }
     }
 }
