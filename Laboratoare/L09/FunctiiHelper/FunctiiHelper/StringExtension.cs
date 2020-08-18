802f02c6a082cd752a1047eab7074c59185b0b44
Ovidiu Rudi <djvidov@yahoo.com>
2020-08-18 21:28:24 +0300

    am lucrat la laboratorul 09

---
+++ Laboratoare/L09/FunctiiHelper/FunctiiHelper/StringExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctiiHelper
+{
+    public static class StringExtension
+    {
+        public static string CipCriptare(this string str, string cheie)
+        {
+            var arrayText = str.ToArray();
+            string result = string.Empty;
+            foreach (var ch in arrayText)
+            {
+                result += ch + cheie;
+            }
+            return result;
+        }
+    }
+}
