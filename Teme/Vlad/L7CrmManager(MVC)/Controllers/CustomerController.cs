e41c2120e389bf22654459e78c5c5b9c38468ce3
Vlad Dolinschi <vlad.dolinschi@gmail.com>
2020-08-18 18:29:04 +0300

    primul MVC ( fara pct 4 - paging)

---
+++ Teme/Vlad/L7CrmManager(MVC)/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using L7CrmManager_MVC_.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using L7CrmManager_MVC_.Models;
+
+namespace L7CrmManager_MVC_.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            CRMEntities db = new CRMEntities();
+            ICollection<Customer> customers = db.Customers.ToList();
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int id) 
+        {
+            CRMEntities db = new CRMEntities();
+            Customer model = db.Customers.Find(id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            return View(model);
+
+        }
+
+    }
+
+}
---
+++ Teme/Vlad/L7CrmManager(MVC)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="L7CrmManager_MVC_.MvcApplication" Language="C#" %>
