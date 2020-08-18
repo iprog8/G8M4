1eca965284014810c106605526f6a237de9c547b
Vlad Dolinschi <vlad.dolinschi@gmail.com>
2020-08-18 18:29:04 +0300

    primul MVC ( fara pct 4 - paging)

---
+++ Teme/Vlad/L7CrmManager(MVC)/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(L7CrmManager_MVC_.Startup))]
+namespace L7CrmManager_MVC_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
+
