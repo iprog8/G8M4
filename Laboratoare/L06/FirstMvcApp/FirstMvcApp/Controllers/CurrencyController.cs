028dd16be9a538b09f07a8a8f6af81f0f5f9258a
Ovidiu Rudi <djvidov@yahoo.com>
2020-08-20 21:43:18 +0300

    am lucrat la laboratorul 10

---
+++ Laboratoare/L06/FirstMvcApp/FirstMvcApp/Controllers/CurrencyController.cs
@@ -0,0 +1,20 @@
+﻿using FirstMvcApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMvcApp.Controllers
+{
+    public class CurrencyController : Controller
+    {
+        // GET: Currency
+        public ActionResult _CursValutar()
+        {
+            var db = new CRMEntities();
+            var model = db.Currencies.ToList();
+            return PartialView(model);
+        }
+    }
+}
