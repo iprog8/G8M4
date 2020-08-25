f48f6360727971cc4993c7709a65ba4b2bc58596
Vlad Dolinschi <vlad.dolinschi@gmail.com>
2020-08-25 18:25:05 +0300

    language route

--- Teme/Vlad/L7CrmManager(MVC)/App_Start/RouteConfig.cs
+++ Teme/Vlad/L7CrmManager(MVC)/App_Start/RouteConfig.cs
@@ -9,14 +9,16 @@ namespace L7CrmManager_MVC_
 {
     public class RouteConfig
     {
+        /*Sa modificati ruta astfel incat sa adaugati si limba in url si la pagini sa faceti ca textul din header sa fie afisat in limba din url
+        Sa creati un nou layout si sa il customizati asa cum vreti voi si sa il folositi pe view-urile din Supplier sau din Customer;*/
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{lang}/{controller}/{action}/{id}",
+                defaults: new { lang = "ro",controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
