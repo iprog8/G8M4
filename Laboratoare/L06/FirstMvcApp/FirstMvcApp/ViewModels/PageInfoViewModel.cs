852a1eb7bbeb9bb57f1c9da1fa1df4426c23a504
Ovidiu Rudi <djvidov@yahoo.com>
2020-08-25 21:15:38 +0300

    am lucrat la laboratorul 11

---
+++ Laboratoare/L06/FirstMvcApp/FirstMvcApp/ViewModels/PageInfoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstMvcApp.ViewModels
+{
+    public class PageInfoViewModel
+    {
+        public int CurrentPage { get; set; } = 1;
+        public double MaxPage { get; set; }
+        public string ViewName { get; set; } = "Index";
+    }
+}
