8b02a862bd6335c349cc18d03b23c2109769231a
Vlad Dolinschi <vlad.dolinschi@gmail.com>
2020-08-27 20:57:43 +0300

    customer pagination

---
+++ Laboratoare/L06/FirstMvcApp/FirstMvcApp/ViewModels/CustomerIndexViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstMvcApp.ViewModels
+{
+    public class CustomerIndexViewModel
+    {
+        public CustomerIndexViewModel()
+        {
+            CustomerList = new List<CustomerViewModel>();
+            PageInfo = new PageInfoViewModel();
+        }
+        public ICollection<CustomerViewModel> CustomerList { get; set; }
+        public PageInfoViewModel PageInfo { get; set; }
+
+    }
+}
