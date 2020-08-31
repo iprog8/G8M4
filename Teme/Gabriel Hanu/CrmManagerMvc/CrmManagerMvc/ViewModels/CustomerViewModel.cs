115f5d5dbadc72e919e999b5f802e02495312669
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2020-08-31 18:49:40 +0300

    add pagging for customers and suppliers

---
+++ Teme/Gabriel Hanu/CrmManagerMvc/CrmManagerMvc/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrmManagerMvc.ViewModels
+{
+    public class CustomerViewModel
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ Teme/Gabriel Hanu/CrmManagerMvc/CrmManagerMvc/ViewModels/SupplierViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrmManagerMvc.ViewModels
+{
+    public class SupplierViewModel
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+    }
+}
