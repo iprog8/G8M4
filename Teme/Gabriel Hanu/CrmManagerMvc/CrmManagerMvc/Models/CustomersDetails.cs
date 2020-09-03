c1523f05e396b903a78b22231d2299c0a193ee92
HiMyNameIsGarch <gabrielhnau700@yahoo.com>
2020-09-03 17:34:08 +0300

    add translation on customers, supp &  partialviews

--- Teme/Gabriel Hanu/CrmManagerMvc/CrmManagerMvc/Models/CustomersDetails.cs
+++ Teme/Gabriel Hanu/CrmManagerMvc/CrmManagerMvc/Models/CustomersDetails.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CrmManagerMvc.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,5 +9,6 @@ namespace CrmManagerMvc.Models
     public class CustomersDetails
     {
         public int TotalCustomers { get; set; }
+        public TranslationViewModel Translations { get; set; } = new TranslationViewModel();
     }
 }
