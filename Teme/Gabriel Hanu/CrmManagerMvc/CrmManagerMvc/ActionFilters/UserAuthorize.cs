7a29cbbefdaafcd04e7469c97dd578c344ea3913
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2020-09-07 18:13:33 +0300

    custom user authentification

---
+++ Teme/Gabriel Hanu/CrmManagerMvc/CrmManagerMvc/ActionFilters/UserAuthorize.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Razor.Parser;
+using System.Web.Routing;
+
+namespace CrmManagerMvc.ActionFilters
+{
+    public class UserAuthorize: AuthorizeAttribute
+    {
+        private string Theme { get; set; }
+        private string Lang { get; set; }
+        private string Controller { get; set; }
+        private string Action { get; set; }
+        private string Id { get; set; }
+        private string UrlForReturn { get; set; }
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            GetUrlValues(filterContext);
+            if (!filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new
+                {
+                    controller = "Account",
+                    action = "Login",
+                    ReturnUrl = UrlForReturn
+                }));
+               // base.HandleUnauthorizedRequest(filterContext);
+            }
+            else
+            {
+                filterContext.Result = new RedirectToRouteResult(new
+                RouteValueDictionary(new { controller = Controller, action = Action }));
+            }
+        }
+        private void GetUrlValues(AuthorizationContext authorizationContext)
+        {
+            var routeData = authorizationContext.RequestContext.RouteData;
+            Theme = (string)routeData.Values["theme"];
+            Lang = (string)routeData.Values["language"];
+            Controller = (string)routeData.Values["controller"];
+            Action = (string)routeData.Values["action"];
+            Id = (string)routeData.Values["id"];
+            UrlForReturn = $"/{Theme}/{Lang}/{Controller}/{Action}";
+            if (Id != null) UrlForReturn += $"/{Id}";
+        }
+    }
+}
--- Teme/Gabriel Hanu/CrmManagerMvc/CrmManagerMvc/Controllers/CustomerController.cs
+++ Teme/Gabriel Hanu/CrmManagerMvc/CrmManagerMvc/Controllers/CustomerController.cs
@@ -1,9 +1,12 @@
-﻿using CrmManagerMvc.Models;
+﻿using CrmManagerMvc.ActionFilters;
+using CrmManagerMvc.Models;
 using CrmManagerMvc.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,6 +14,7 @@ namespace CrmManagerMvc.Controllers
 {
     public class CustomerController : Controller
     {
+        private CRMEntities db = new CRMEntities();
         // GET: Customer
         public ActionResult Index(int page = 1)
         {
@@ -53,6 +57,98 @@ namespace CrmManagerMvc.Controllers
             model.TotalCustomers = db.Customers.Count();
             model.Translations.GetTranslations(db, this.RouteData);
             return PartialView(model);
+        }
+        // GET: Customers/Create
+        [UserAuthorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [UserAuthorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,City,Country,Phone")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
+        // GET: Customers/Edit/5
+        [UserAuthorize]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [UserAuthorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,City,Country,Phone")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        [UserAuthorize]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [UserAuthorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customer customer = db.Customers.Find(id);
+            db.Customers.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
