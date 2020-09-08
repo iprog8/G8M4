cb3e603809d86dae9a7fcd1738e8190f79f126d9
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2020-09-08 17:23:40 +0300

    change theme/language from navbar

---
+++ Teme/Gabriel Hanu/CrmManagerMvc/CrmManagerMvc/Controllers/LanguageController.cs
@@ -0,0 +1,21 @@
+﻿using CrmManagerMvc.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrmManagerMvc.Controllers
+{
+    public class LanguageController : Controller
+    {
+        // GET: Language
+        public ActionResult _LanguagePartial()
+        {
+            LanguageViewModel model = new LanguageViewModel();
+            model.GetValues(HttpContext.Request.RawUrl, this.RouteData);
+            return PartialView(model);
+        }
+    }
+}
---
+++ Teme/Gabriel Hanu/CrmManagerMvc/CrmManagerMvc/Enums/RouteTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrmManagerMvc.Enums
+{
+    public enum RouteTypes
+    {
+        Theme = 1,
+        Language = 2
+    }
+}
