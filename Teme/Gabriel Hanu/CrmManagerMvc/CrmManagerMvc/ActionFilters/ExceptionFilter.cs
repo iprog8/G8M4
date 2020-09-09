ee744803e412ed53c47da5c747774e6a9da06f98
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2020-09-09 20:46:07 +0300

    add global exception filter

---
+++ Teme/Gabriel Hanu/CrmManagerMvc/CrmManagerMvc/ActionFilters/ExceptionFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace CrmManagerMvc.ActionFilters
+{
+    public class ExceptionFilter: FilterAttribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new
+            {
+                controller = "Home",
+                action = "Error"
+            }));
+        }
+    }
+}
