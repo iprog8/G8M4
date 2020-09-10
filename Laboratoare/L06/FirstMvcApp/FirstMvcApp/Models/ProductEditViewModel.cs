f09bde4a72b4b8e5fdfbadfac53a55373c95d47e
Ovidiu Rudi <djvidov@yahoo.com>
2020-09-10 22:20:46 +0300

    am lucrat la laboratorul 16

---
+++ Laboratoare/L06/FirstMvcApp/FirstMvcApp/Models/ProductEditViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMvcApp.Models
+{
+    public class ProductEditViewModel
+    {
+        public Product Product { get; set; }
+        public SelectList Suppliers { get; set; }
+    }
+}
