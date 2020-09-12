f8f7c111458de3c129490633d129ad111f2c32fc
Ovidiu Rudi <djvidov@yahoo.com>
2020-09-12 17:54:36 +0300

    pagina de lista articole

---
+++ Laboratoare/WoC/Blog/Blog/ViewModels/ArticolIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.ViewModels
+{
+    public class ArticolIndexViewModel
+    {
+        public ICollection<ArticolViewModel> ListaArticole { get; set; } = new List<ArticolViewModel>();
+        public PaginareViewModel Paginare { get; set; } = new PaginareViewModel();
+    }
+}
---
+++ Laboratoare/WoC/Blog/Blog/ViewModels/PaginareViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.ViewModels
+{
+    public class PaginareViewModel
+    {
+        public int ItemsPerPage { get; set; } = 10;
+        public int CurentPage { get; set; }
+        public int MaxPage { get; set; }
+        public void GetMaxPage(double totalItems)
+        {
+            MaxPage = (int)Math.Ceiling(totalItems / (double)ItemsPerPage);
+        }
+    }
+}
