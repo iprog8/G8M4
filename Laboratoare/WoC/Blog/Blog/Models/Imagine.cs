668027bca1240b38fdd5599305d5c07d212c5798
Ovidiu Rudi <djvidov@yahoo.com>
2020-09-12 14:26:53 +0300

    gestiune imagini

---
+++ Laboratoare/WoC/Blog/Blog/Models/Imagine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class Imagine
+    {
+        public const string CaleImagini = "/imagini-articol";
+        public string Cale { get; set; }
+        public byte[] Poza { get; set; }
+    }
+}
