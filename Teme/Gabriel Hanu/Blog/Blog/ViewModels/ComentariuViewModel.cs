3037a6f2e52f6cbbf5641e8f85b315611931eba6
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2020-09-13 08:41:30 +0300

    single article details

---
+++ Teme/Gabriel Hanu/Blog/Blog/ViewModels/ComentariuViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.ViewModels
+{
+    public class ComentariuViewModel
+    {
+        public int Id { get; set; }
+        public string Nume { get; set; }
+        public string Titlu { get; set; }
+        public string Text { get; set; }
+        public DateTime DataCreare { get; set; }
+        public bool Edited { get; set; }
+        public bool Aprobat { get; set; }
+    }
+}
