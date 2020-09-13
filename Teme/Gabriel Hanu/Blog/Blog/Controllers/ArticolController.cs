abfc0fb851c999ca0ad19c11e88b013e8154d988
HiMyNameIsGarch <gabrielhnau700@yahoo.com>
2020-09-13 08:41:30 +0300

    single article details

---
+++ Teme/Gabriel Hanu/Blog/Blog/Controllers/ArticolController.cs
@@ -0,0 +1,45 @@
+﻿using Blog.Models;
+using Blog.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace Blog.Controllers
+{
+    public class ArticolController : Controller
+    {
+        // GET: Articol
+        public ActionResult Index(int page = 1)
+        {
+            return View();
+        }
+        public ActionResult Details(int id)
+        {
+            BlogEntities db = new BlogEntities();
+            ArticolViewModel model = db.Postares.Include(a => a.Pozas)
+                .Include(p => p.Comentarius)
+                .Select((p => new ArticolViewModel
+                {
+                    Id = p.Id,
+                    Titlu = p.Titlu,
+                    DataCreare = p.DataCreare,
+                    Text = p.Text,
+                    ListaComentarii = p.Comentarius.Select(c => new ComentariuViewModel { 
+                        Id = c.Id,
+                        DataCreare = c.DataCreare,
+                        Aprobat = c.Aprobat,
+                        Edited = c.Edited,
+                        Nume = c.Nume,
+                        Text = c.Text,
+                        Titlu = c.Titlu
+                    }).ToList(),
+                    ListaPoze = p.Pozas
+                }))
+                .FirstOrDefault(p => p.Id == id);
+            return View(model);
+        }
+    }
+}
