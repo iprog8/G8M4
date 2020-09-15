ff6ee04757c440d045dcee4c7171773e437c3742
Ovidiu Rudi <djvidov@yahoo.com>
2020-09-15 22:07:00 +0300

    am lucrat la laboratorul 17

---
+++ Laboratoare/L17/FirstApi/Api2/ViewModels/CustomerVM.cs
@@ -0,0 +1,32 @@
+﻿using Api2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api2.ViewModels
+{
+    public class CustomerVM
+    {
+        public CustomerVM()
+        {
+        }
+
+        public CustomerVM(Customer customer)
+        {
+            this.Id = customer.Id;
+            this.City = customer.City;
+            this.Country = customer.Country;
+            this.FirstName = customer.FirstName;
+            this.LastName = customer.LastName;
+            this.Phone = customer.Phone;
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+    }
+
+}
