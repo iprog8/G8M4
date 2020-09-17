419634bbc0696c39905d1bf7f8e3f0077582779f
Ovidiu Rudi <djvidov@yahoo.com>
2020-09-17 19:16:45 +0300

    tema luata de a Gabi

--- Laboratoare/L17/FirstApi/Api2/Models/ViewModelAdapter.cs
+++ Laboratoare/L17/FirstApi/Api2/Models/ViewModelAdapter.cs
@@ -22,4 +22,17 @@ namespace Api2.Models
 
     }
 
+    public partial class Supplier
+    {
+        public Supplier(SupplierViewModel supplierVM)
+        {
+            this.Id = supplierVM.Id;
+            this.CompanyName = supplierVM.CompanyName;
+            this.ContactName = supplierVM.ContactName;
+            this.ContactTitle = supplierVM.ContactTitle;
+            this.City = supplierVM.City;
+            this.Country = supplierVM.Country;
+            this.Phone = supplierVM.Phone;
+        }
+    }
 }
