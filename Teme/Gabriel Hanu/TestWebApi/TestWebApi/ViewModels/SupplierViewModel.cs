9b2127b65fef9b79c5ee9abce1f78acd0f225853
HiMyNameIsGarch <gabrielhnau700@yahoo.com>
2020-09-17 12:16:54 +0300

    tema cu posibila eroare

---
+++ Teme/Gabriel Hanu/TestWebApi/TestWebApi/ViewModels/SupplierViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using TestWebApi.Models;
+
+namespace TestWebApi.ViewModels
+{
+    public class SupplierViewModel
+    {
+        public SupplierViewModel()
+        {
+
+        }
+        public SupplierViewModel(Supplier supplier)
+        {
+           this.Id = supplier.Id;
+           this.CompanyName = supplier.CompanyName;
+           this.ContactName = supplier.ContactName;
+           this.ContactTitle = supplier.ContactTitle;
+           this.City = supplier.City;
+           this.Country = supplier.Country;
+           this.Phone = supplier.Phone;
+        }
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string ContactTitle { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+    }
+}
